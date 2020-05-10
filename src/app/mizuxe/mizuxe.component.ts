3aca3f0ab39593bb5d32338de050c630bc0377c7
gajapathirao <gajapathiraoguummadi@gmail.com>
2020-05-10 13:42:43 +0530

    complate commit

--- src/app/mizuxe/mizuxe.component.ts
+++ src/app/mizuxe/mizuxe.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mizuxe',
   templateUrl: './mizuxe.component.html',
-  styleUrls: ['./mizuxe.component.scss']
+  styleUrls: ['./mizuxe.component.scss'],
 })
 export class MizuxeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  year = new Date().getFullYear();
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  scrollToElement(element): void {
+    console.log(element);
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
   }
-
 }
